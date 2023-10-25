0af46ca107c3a32e7e595300f781f32f2cd20d3c
tumo2208 <22028021@vnu.edu.vn>
2023-10-25 13:26:34 +0700

    rework

--- src/main/java/dictionary/elearnapp_javafx_group8/Controller/AppUIController.java
+++ src/main/java/dictionary/elearnapp_javafx_group8/Controller/AppUIController.java
@@ -1,4 +1,19 @@
 package dictionary.elearnapp_javafx_group8.Controller;
 
+import javafx.scene.control.*;
+
 public class AppUIController {
+
+    public Button addNewWordButton;
+    public TextArea newDefinitionArea;
+    public TextField newWordField;
+
+    public TextField searchField;
+    public Button deleteSearchButton;
+    public ListView<String> listView;
+    public Label wordLabel;
+    public Button listenButton;
+    public Button editWordButton;
+    public Button deleteWordButton;
+    public TextArea definitionArea;
 }
