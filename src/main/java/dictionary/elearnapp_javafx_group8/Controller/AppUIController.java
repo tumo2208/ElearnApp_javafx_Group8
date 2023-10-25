31a35f8afec149a82b9a715074242dc80c00f5f9
tumo2208 <22028021@vnu.edu.vn>
2023-10-25 13:39:28 +0700

    rework

--- src/main/java/dictionary/elearnapp_javafx_group8/Controller/AppUIController.java
+++ src/main/java/dictionary/elearnapp_javafx_group8/Controller/AppUIController.java
@@ -1,8 +1,18 @@
 package dictionary.elearnapp_javafx_group8.Controller;
 
+import dictionary.elearnapp_javafx_group8.Dictionary.Dictionary;
+import dictionary.elearnapp_javafx_group8.Dictionary.Word;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
-public class AppUIController {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class AppUIController implements Initializable {
 
     public Button addNewWordButton;
     public TextArea newDefinitionArea;
@@ -16,4 +26,45 @@ public class AppUIController {
     public Button editWordButton;
     public Button deleteWordButton;
     public TextArea definitionArea;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        dictionary.insertFromFile(wordList);
+        dictionary.setTrie(wordList);
+        listViewDefault();
+        searchField.setOnKeyTyped(keyEvent -> {
+            if (searchField.getText().isEmpty()){
+                deleteSearchButton.setVisible(false);
+                listViewDefault();
+            } else {
+                deleteSearchButton.setVisible(true);
+                observableList.clear();
+                String searchWord = searchField.getText().trim();
+                observableList = dictionary.Lookup(searchWord);
+                if (observableList.isEmpty()) {
+                    listViewDefault();
+                } else {
+                    listView.setItems(observableList);
+                }
+            }
+        });
+
+        deleteSearchButton.setOnAction(event -> {
+            searchField.clear();
+            deleteSearchButton.setVisible(false);
+            listViewDefault();
+        });
+    }
+
+    private void listViewDefault(){
+        observableList.clear();
+        for (int i = 0; i < 20; i++) {
+            observableList.add(wordList.get(i).getWordTarget());
+        }
+        listView.setItems(observableList);
+    }
+
+    private List<Word> wordList = new ArrayList<>();
+    private Dictionary dictionary = new Dictionary();
+    private ObservableList<String> observableList = FXCollections.observableArrayList();
 }
