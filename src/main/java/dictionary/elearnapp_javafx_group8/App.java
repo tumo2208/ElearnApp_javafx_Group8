4b07efc77f70dd63b4ce641f6d190aa775f7f58d
xcviixix-rei <124432779+xcviixix-rei@users.noreply.github.com>
2023-10-25 14:14:22 +0700

    Menu bar update button

--- src/main/java/dictionary/elearnapp_javafx_group8/App.java
+++ src/main/java/dictionary/elearnapp_javafx_group8/App.java
@@ -10,7 +10,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("English Learning Application");
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/FXML/Menu.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/FXML/AppUI.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
