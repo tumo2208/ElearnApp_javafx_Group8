18752fd6c83f8be680558a2605d8868e3af247f6
tumo2208 <tuadc1234@gmail.com>
2023-11-24 11:18:22 +0700

    improve ui game and fix api

--- src/main/java/dictionary/elearnapp_javafx_group8/Controller/APIController.java
+++ src/main/java/dictionary/elearnapp_javafx_group8/Controller/APIController.java
@@ -35,6 +35,8 @@ public class APIController implements Initializable {
     public Button translateButton;
     public Label limitLetters;
     public ImageView loadingImg;
+    public ImageView errorTranslate;
+    public ImageView noConnection;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -50,10 +52,14 @@ public class APIController implements Initializable {
         limitLetters.setText("0 / 1000");
         secondLanguageArea.setEditable(false);
         loadingImg.setVisible(false);
+        errorTranslate.setVisible(false);
+        noConnection.setVisible(false);
         resetAllAPI();
 
         firstLanguageArea.setOnKeyTyped(keyEvent -> {
             secondLanguageArea.clear();
+            errorTranslate.setVisible(false);
+            noConnection.setVisible(false);
             translateButton.setDisable(firstLanguageArea.getText().isEmpty());
             limitLetters.setText(firstLanguageArea.getText().length() + " / 2000");
         });
@@ -63,6 +69,8 @@ public class APIController implements Initializable {
             secondLanguageArea.clear();
             translateButton.setDisable(true);
             loadingImg.setVisible(false);
+            errorTranslate.setVisible(false);
+            noConnection.setVisible(false);
             if (sourceLanguage.equals("en")) {
                 sourceLanguage = "vi";
                 toLanguage = "en";
@@ -84,15 +92,24 @@ public class APIController implements Initializable {
             try {
                 if (firstLanguageArea.getText().length() <= 2000) {
                     googleTranslate();
-                    loadingImg.setVisible(true);
-                    secondLanguageArea.setVisible(false);
-                    countDownTimeLine.setOnFinished(event1 -> {
-                        loadingImg.setVisible(false);
-                        secondLanguageArea.setVisible(true);
-                    });
-                    countDownTimeLine.play();
+                    if (hasConnect) {
+                        loadingImg.setVisible(true);
+                        secondLanguageArea.setVisible(false);
+                        countDownTimeLine.setOnFinished(event1 -> {
+                            if (!secondLanguageArea.getText().isEmpty()) {
+                                loadingImg.setVisible(false);
+                                secondLanguageArea.setVisible(true);
+                            }
+                            else {
+                                loadingImg.setVisible(false);
+                                errorTranslate.setVisible(true);
+                            }
+                        });
+                        countDownTimeLine.play();
+                    }
                 } else {
-                    secondLanguageArea.setText("ERROR TO TRANSLATE");
+                    secondLanguageArea.clear();
+                    errorTranslate.setVisible(true);
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -101,41 +118,50 @@ public class APIController implements Initializable {
         });
     }
 
-    private void googleTranslate() throws IOException {
+    private void googleTranslate() {
         String rootAPI = "https://translate.googleapis.com/translate_a/single?client=gtx"
                 + "&sl=" + sourceLanguage
                 + "&tl=" + toLanguage
                 + "&dt=t&q=";
         String textToTranslate = firstLanguageArea.getText();
-        textToTranslate=textToTranslate.replace("\"", "\'\'");
-        textToTranslate=textToTranslate.replace("],[","], [");
-        String URLString = rootAPI + URLEncoder.encode(textToTranslate,"UTF-8");
-        URLString = URLString.replace(" ", "%20");
-        URL url = new URL(URLString);
-        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-        InputStream stream;
-        if (httpURLConnection.getResponseCode() == 200) {
-            stream = httpURLConnection.getInputStream();
-        } else {
-            stream = httpURLConnection.getErrorStream();
-        }
-        BufferedReader in = new BufferedReader(new InputStreamReader(stream));
-        String line;
-        String translateText = "";
-        line = in.readLine();
-        line = line.replace("824257d7a249c58caeea06a2e64a25bd", "");
-        line = line.replace("07cf6d00dc83dfc068bf115b58e01f7f", "");
-        for (String str : line.split("\\],\\[")) {
-            if (str.contains("\"")) {
-                String[] outs = str.split("\"");
-                translateText += outs[1];
+        textToTranslate = textToTranslate.replace("\"", "\'\'");
+        textToTranslate = textToTranslate.replace("],[", "], [");
+
+        try {
+            String URLString = rootAPI + URLEncoder.encode(textToTranslate, "UTF-8");
+            URLString = URLString.replace(" ", "%20");
+            URL url = new URL(URLString);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream stream = null;
+            if (httpURLConnection.getResponseCode() == 200) {
+                stream = httpURLConnection.getInputStream();
+            } else {
+                stream = httpURLConnection.getErrorStream();
             }
+            BufferedReader in = new BufferedReader(new InputStreamReader(stream));
+            String line;
+            String translateText = "";
+            line = in.readLine();
+            line = line.replace("824257d7a249c58caeea06a2e64a25bd", "");
+            line = line.replace("07cf6d00dc83dfc068bf115b58e01f7f", "");
+            for (String str : line.split("\\],\\[")) {
+                if (str.contains("\"")) {
+                    String[] outs = str.split("\"");
+                    translateText += outs[1];
+                }
+            }
+            in.close();
+            httpURLConnection.disconnect();
+            translateText = translateText.translateEscapes();
+            translateText = translateText.replace("], [", "],[");
+            secondLanguageArea.setText(translateText);
+            hasConnect = true;
+        } catch (Exception e) {
+            hasConnect = false;
+            secondLanguageArea.clear();
+            loadingImg.setVisible(false);
+            noConnection.setVisible(true);
         }
-        in.close();
-        httpURLConnection.disconnect();
-        translateText = translateText.translateEscapes();
-        translateText=translateText.replace("], [","],[");
-        secondLanguageArea.setText(translateText);
     }
 
     private void resetAllAPI() {
@@ -152,6 +178,7 @@ public class APIController implements Initializable {
     private String toLanguage = "vi";
     private Image vietnamFlag;
     private Image englandFlag;
+    private boolean hasConnect = true;
     private final ObjectProperty<Duration> remainingDuration
             = new SimpleObjectProperty<>(java.time.Duration.ofSeconds(5));
     private final Timeline countDownTimeLine = new Timeline(new KeyFrame(javafx.util.Duration.seconds(5),
